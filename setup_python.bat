22312bb73753bbb21dab105255b165ea881184af
Edo758 <edoardo09colognato@gmail.com>
2025-06-14 22:33:17 +0200

    Rimuovo il file .bat perché non più necessario

--- setup_python.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Imposta la cartella corrente dove si trova il .bat (e il .ps1)
-set SCRIPT_DIR=%~dp0
-
-REM Esegue il file PowerShell in modalità bypass per evitare restrizioni
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_DIR%setup_python.ps1"
-
-pause
