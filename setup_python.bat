ef487208bd5420abd006b973afd96f16d35dd5b9
Edo758 <edoardo09colognato@gmail.com>
2025-06-14 16:58:43 +0200

    Script di Setup automatico Python

--- setup_python.bat
+++ setup_python.bat
@@ -1,1 +1,8 @@
-powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Users\ASUS\Documents\SpotifyBot\setup_python.ps1" pause
+@echo off
+REM Percorso della cartella dove si trova il batch (e il file .ps1)
+set SCRIPT_DIR=%~dp0
+
+REM Esegui lo script PowerShell con esecuzione bypassata per policy (solo per questo comando)
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_DIR%setup_python.ps1"
+
+pause
