c7a9aebf7b5af6010b5c006b9fd30db4ec8c5aaa
Edo758 <edoardo09colognato@gmail.com>
2025-06-24 12:19:40 +0200

    Trova le coordinate sullo schermo

--- Trova Coordinate Fast.py
+++ Trova Coordinate Fast.py
@@ -1,9 +1,18 @@
 from pynput import mouse
+import time
 
 def on_click(x, y, button, pressed):
     if pressed:
         print(f"Cliccato a posizione: x={x}, y={y}")
 
-print("Fai click per vedere le coordinate.")
-with mouse.Listener(on_click=on_click) as listener:
-    listener.join()
+print("Fai click per vedere le coordinate. Premi Ctrl+C per uscire.")
+
+listener = mouse.Listener(on_click=on_click)
+listener.start()
+
+try:
+    while True:
+        time.sleep(0.1)  # mantiene vivo lo script senza bloccare il main thread
+except KeyboardInterrupt:
+    print("\nInterruzione richiesta")
+    listener.stop()
